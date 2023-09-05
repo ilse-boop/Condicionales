0c03b225d06919a7d277ce0d9600ea6afa324740
ilse-boop <ilsemarneftlimoncorro@gmail.com>
2023-09-05 09:51:52 -0600

    se agrega el proyecto

---
+++ src/main/java/com/mycompany/condicionales/Condicionales.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.condicionales;
+
+/**
+ *
+ * @author EQUIPO
+ */
+public class Condicionales {
+
+    public static void main(String[] args) {
+       int edad = 20;
+        
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
+    }
+}
